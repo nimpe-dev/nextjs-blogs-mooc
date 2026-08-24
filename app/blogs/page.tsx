750fa1f321d35e776bc725bd85b08cb870b3e54d
nimpeboss <azawi.rivan@counciltech.org>
2026-08-24 18:26:26 +0300

    exercises 5-6: sorting and search

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,20 +1,37 @@
 import Link from "next/link";
 import { getBlogs } from "../services/blogs";
 
-const Blogs = () => {
-    const blogs = getBlogs()
-    return (
-        <div>
-            <h2>Blogs</h2>
-            <ul>
-                {blogs.map((blog) => (
-                    <li key={blog.id}>
-                        <Link href={`/blogs/${blog.id}`}>{blog.title}</Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+const Blogs = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ filter?: string }>;
+}) => {
+  const { filter } = await searchParams;
+  const allBlogs = [...getBlogs()].sort((a, b) => b.likes - a.likes);
 
-export default Blogs
+  const blogs = filter
+    ? allBlogs.filter((blog) =>
+        blog.title.toLowerCase().includes(filter.toLowerCase()),
+      )
+    : allBlogs;
+
+  return (
+    <div>
+      <h2>Blogs</h2>
+      <form action="/blogs">
+        <input type="text" name="filter" defaultValue={filter ?? ""} />
+        <button type="submit">Search</button>
+      </form>
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blogs/${blog.id}`}>{blog.title}</Link> by{" "}
+            {blog.author}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Blogs;
